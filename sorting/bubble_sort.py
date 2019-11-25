c1a9be7263cfe86405776db385680ad159a0ec32
Bharath Narayan M G <bharath144@gmail.com>
2019-11-25 07:33:02 +0530

    Update bubble_sort.py
    Added recursive bubble sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,23 +1,48 @@
 import random
 import time
 
-input_a = [x for x in range(0, 15)]
+input_a = [x for x in range(0, 1000)]
 input_b = random.sample(input_a, 10)
-print(input_b)
+input_c = input_b.copy()
 
-for i in range(0, len(input_b)):
-    # Improved exit condition, if there are no swaps, all numbers are sorted
-    swapped = False
-    for j in range(0, len(input_b) - i - 1):
-        if input_b[j] > input_b[j+1]:
-            # The actual in-place swap, a semantic that's unique to Python.
-            # This is similar to temp = x, x = y, y = temp
-            input_b[j], input_b[j+1] = input_b[j+1], input_b[j]
-            print("   " + str(input_b))
+def iterative_sort(unsorted_list):
+    print(unsorted_list)
+    for i in range(0, len(unsorted_list)):
+        # Improved exit condition, if there are no swaps, all numbers are sorted
+        swapped = False
+        for j in range(0, len(unsorted_list) - i - 1):
+            if unsorted_list[j] > unsorted_list[j+1]:
+                # The actual in-place swap, a semantic that's unique to Python.
+                # This is similar to temp = x, x = y, y = temp
+                unsorted_list[j], unsorted_list[j+1] = \
+                    unsorted_list[j+1], unsorted_list[j]
+                print("   " + str(unsorted_list))
 
-            # Indicates if we swapped something
-            swapped = True
-            time.sleep(1)
-    print(input_b)
-    if not swapped:
-        break
+                # Indicates if we swapped something
+                swapped = True
+        print(unsorted_list)
+        if not swapped:
+            break
+
+def recurseive_sort(unsorted_list):
+    print(unsorted_list)
+    for i, num in enumerate(unsorted_list):
+        try:
+            if unsorted_list[i + 1] < num:
+                # Swap
+                unsorted_list[i] = unsorted_list[i + 1]
+                unsorted_list[i + 1] = num
+                print("   " + str(unsorted_list))
+                recurseive_sort(unsorted_list)
+        except IndexError:
+            print(unsorted_list)
+            pass
+
+    return unsorted_list
+
+recurseive_sort(input_b)
+print("")
+print("")
+print("")
+print("")
+iterative_sort(input_c)
